1c399565219a5f5475a8f2be7877dfde180ad140
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2023-12-19 10:36:41 +0500

    finish project

---
+++ portfolio_page/page1/js/lang.js
@@ -0,0 +1,187 @@
+const langData = {
+    "uz": {
+      "home": "Sayt yaratish",
+      "about": "SEO va Daromadni Oshirish",
+      "product": "Dizayn va Brending",
+      "our_work": "Ish namunalarimiz",
+      "price": "Narxlar",
+      "nav_text": "Buyurtma berish",
+      "service_text1": "SEO xizmatida kutilmagan natija",
+      "service_text2": "Ko'rsatilgan xizmat",
+      "service_text3": "SEO",
+      "service_text4": "Mijoz saytini ko'ring",
+      "service_text5": "Bizning xizmatlarimiz",
+      "service_text6": "Sayt Yaratish",
+      "service_text7": "Internet do'kon yaratish",
+      "service_text8": "SEO va Googleda reklama",
+      "service_text9": "SMM",
+      "service_text10": "Logotip yasash",
+      "service_text11": "Brend nomlarini topsih",
+      
+      "footer_text1.1": "Biz 100 dan ortiq mijozlarimizni xursand qildik.", 
+      "footer_text1.2": "Sizga qanday yordam berishimiz mumkinligini bilish uchun biz bilan bog'laning!", 
+      "footer_text2": "Yuz marta o'qigandan ko'ra, bir marta gaplashish yaxshiroqdir. Sizning biznesingizni tahlil qilaylik va siz uchun maxsus taklif tayyorlaylik.", 
+      "footer_text3.1": "O'zingizning biznesingizni targ'ib qilishni ", 
+      "footer_text3.2": " kechiktirmang, aks holda kimdir bu imkoniyatdan foydalanadi.", 
+      "footer_text4": "Bizga hozir qo'ng'iroq qiling:", 
+      "footer_text5": "Aloqalar", 
+      "footer_text6": "Kattaqo'rg'on ko'ch. 20-uy, Toshkent Shahar",
+      "footer_text7": "Menyu",
+      "footer_text8": "Qadriyatlarimiz",
+      "footer_text9": "CMD-TECh jamoasi",
+      "footer_text10": "Jamoamizga qo'shiling",
+      "footer_text811": "Aloqalar",
+      "footer_text12": "Biz haqimizda", 
+      "footer_text13": "Maqolalar", 
+      "footer_text14": "Ish namunalarimiz", 
+      "footer_text15": "Mijozlarimiz fikrlari", 
+      "footer_text16": "Loyihalar va nashrlar", 
+      "footer_text17": "Bizni Kuzatib Boring:",
+      "footer_text2.2": "Arizangizni yuboring",      
+      "btn_text1": "Mutaxassis bilan bog'lanish",
+      "btn_text2": "Ismingiz: *",
+      "btn_text3": "Telefon raqamingiz: *",
+      "btn_text4": "Kompaniya nomi: *",
+      "": "",
+      "": "",
+      "": "",
+  
+      
+    },
+    "ru": {
+      "home": "Создать сайт",
+      "about": "SEO и Увеличение дохода",
+      "product": "Дизайн и Брендинг",
+      "our_work": "Портфолио",
+      "price": "Цены",
+      "nav_text": "Разместить заказ",
+      "service_text1": "",
+      "service_text2": "Ko'rsatilgan xizmat",
+      "service_text3": "SEO",
+      "service_text4": "Посмотреть сайт клиента",
+      "service_text5": "Наши сервисы",
+      "service_text6": "Создание сайта",
+      "service_text7": "Создание интернет-магазина",
+      "service_text8": "SEO и реклама в Google",
+      "service_text9": "SMM",
+      "service_text10": "Сделать логотип",
+      "service_text11": "Список торговых марок",
+      
+      "footer_text1.1": "Мы осчастливили более 100 клиентов. ", 
+      "footer_text1.2": "Свяжитесь с нами, чтобы узнать, как мы можем вам помочь!", 
+      "footer_text2": "Лучше один раз сказать, чем сто раз прочитать. Давайте проанализируем ваш бизнес и подготовим для вас специальное предложение.", 
+      "footer_text3.1": "Не откладывайте продвижение своего ", 
+      "footer_text3.2": " бизнеса, иначе этим воспользуется кто-то другой.", 
+      "footer_text4": "Позвоните нам сейчас:", 
+      "footer_text5": "Контакты", 
+      "footer_text6": "Каттакоргон ул. 20, город Ташкент", 
+      "footer_text7": "Меню", 
+      "footer_text8": "Наши ценности",
+      "footer_text9": "Команда CMD-TECH",
+      "footer_text10": "Присоединиться к нашей команде",
+      "footer_text11": "Контакты",
+      "footer_text12": "о нас", 
+      "footer_text13": "Статьи", 
+      "footer_text14": "Наши образцы работ", 
+      "footer_text15": "Мнения наших клиентов", 
+      "footer_text16": "Проекты и публикации", 
+      "footer_text17": "Подписывайтесь на нас:", 
+      "footer_text2.2":"Оставить заявку",
+      "btn_text1": "Свяжитесь с экспертом",
+      "btn_text2": "Ваше имя: *",
+      "btn_text3": "Ваш номер телефона: *",
+      "btn_text4": "Название компании: *",
+      "": "", 
+      "": "", 
+      "": "", 
+      "": "", 
+      "": "", 
+      "": "", 
+  
+  
+  
+  
+    },
+    "en": {
+      "home": "Create a site",
+      "about": "SEO and Monetization",
+      "product": "Design and Branding",
+      "our_work": "Our work samples",
+      "price": "Prices",
+      "nav_text": "Place an order",
+      "service_text1": "Unexpected result in SEO service",
+      "service_text2": "The service provided",
+      "service_text3": "SEO",
+      "service_text4": "View the client site",
+      "service_text5": "Our services",
+      "service_text6": "Site Creation",
+      "service_text7": "Creating an online store",
+      "service_text8": "SEO and advertising on Google",
+      "service_text9": "SMM",
+      "service_text10": "Make a logo",
+      "service_text11": "",
+
+
+      "footer_text1.1": "We have made more than 100 customers happy. ", 
+      "footer_text1.2": "Contact us to find out how we can help you!", 
+      "footer_text2": "It is better to talk once than read a hundred times. Let us analyze your business and prepare a special offer for you.", 
+      "footer_text3.1": "Don't delay promoting your ", 
+      "footer_text3.2": " business, or someone else will take advantage of it.", 
+      "footer_text4": "Call us now:", 
+      "footer_text5": "Connections", 
+      "footer_text6": "Kattakorgon St. 20, Tashkent City",
+      "footer_text7": "Menu",
+      "footer_text8": "Our values",
+      "footer_text9": "CMD-TECh team",
+      "footer_text10": "Join our team",
+      "footer_text811": "Connections",
+      "footer_text12": "about Us", 
+      "footer_text13": "Articles", 
+      "footer_text14": "Our work samples", 
+      "footer_text15": "Our customers' opinions", 
+      "footer_text16": "Projects and publications", 
+      "footer_text17": "Follow us:",
+      "footer_text2.2": "Submit your application",
+      "btn_text1": "Contact an expert",
+      "btn_text2": "Your name: *",
+      "btn_text3": "Your phone number: *",
+      "btn_text4": "Company name: *",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",
+      "": "",  
+    }
+  };
+  
+  const langElements = document.querySelectorAll('.lang');
+  
+  function changeLanguage(language) {
+    selectedLanguage = language;
+    langElements.forEach(el => {
+      el.textContent = langData[selectedLanguage][el.getAttribute('key')];
+    });
+  }
+  
+  
+  
+  // Tilni qayta belgilash
+  let selectedLanguage = 'ru';
+  langElements.forEach(el => {
+    el.textContent = langData[selectedLanguage][el.getAttribute('key')];
+  });
+  
+  const langButtons = document.querySelectorAll('.lang-button');
+  langButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    changeLanguage(btn.dataset.lang);
+    langButtons.forEach(btn => {
+      btn.classList.remove('active_lang');
+    });
+    btn.classList.add('active_lang');
+  });
+  });
---
+++ portfolio_page/page1/js/modal.js
@@ -0,0 +1,23 @@
+var modal = document.getElementById("myModal");
+var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
+var span = document.getElementsByClassName("close")[0];
+
+btn1.onclick = function() {
+  modal.style.display = "block";
+}
+
+btn2.onclick = function() {
+    modal.style.display = "block";
+  }
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
